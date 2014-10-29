aea7e5c96a15944af30833cec0ac1398df50cc2f
mcivico <mcivico@infobosccoma.net>
2014-10-29 17:50:16 +0100

    Afegir les funcions per crear els 3 contenidors

--- testProva.java
+++ testProva.java
@@ -42,4 +42,70 @@ public class testProva {
 
 	}
 
+public static void crearDryVan(Contenidor[] contenidor, int posicio){
+		int  capacitat, nMercaderias;
+		String numSerie, color, estat;		
+		
+		System.out.println("Numero de seire:");
+		numSerie = lector.nextLine();
+		System.out.println("Capacitat:");
+		capacitat = Integer.parseInt(lector.nextLine());
+		System.out.println("Color:");
+		color = lector.nextLine();
+		System.out.println("Quantes mercaderies vols entrar?");
+		nMercaderias = Integer.parseInt(lector.nextLine());
+		if (nMercaderias <= 100){
+		Mercaderias[] mercaderia = new Mercaderias[nMercaderias];
+		crearMercaderias(nMercaderias,capacitat,mercaderia);
+		estat="tancat";
+		contenidor[posicio]= new DryVan(numSerie,estat,capacitat,mercaderia,color);
+		}
+		else {
+			System.out.println("No i caben tantes mercaderias");
+		}
+	}
+	
+	public static void crearCisterna(Contenidor[] contenidor, int posicio){
+		System.out.println(posicio);
+		String numSerie, estat;
+		int capacitat,nMercaderias;
+		
+		System.out.println("Numero de serie:");
+		numSerie=lector.nextLine();
+		System.out.println("Capacitat");
+		capacitat = Integer.parseInt(lector.nextLine());
+		System.out.println("Quantes mercaderias vols entrar?");
+		nMercaderias=Integer.parseInt(lector.nextLine());
+		if(nMercaderias==1){
+			Mercaderias[] mercaderia = new Mercaderias[nMercaderias];
+			crearMercaderias(nMercaderias,capacitat,mercaderia);
+			estat="tancat";
+			contenidor[posicio]=new Cisterna(numSerie,estat,capacitat,mercaderia);
+		}
+		else{
+			System.out.println("No i caben les mercaderias");
+		}
+		
+	}
+	
+	public static void crearRefrigerant(Contenidor[] contenidor, int posicio){
+		System.out.println("Numero de serie:");
+		String numSerie=lector.nextLine();
+		System.out.println("Capacitat");
+		int capacitat = Integer.parseInt(lector.nextLine());
+		System.out.println("Temperatura");
+		int temperatura=Integer.parseInt(lector.nextLine());
+		System.out.println("Quantes mercaderias vols entrar?");
+		int nMercaderias=Integer.parseInt(lector.nextLine());
+		if(nMercaderias<=100){
+			Mercaderias[] mercaderia = new Mercaderias[nMercaderias];
+			crearMercaderias(nMercaderias,capacitat,mercaderia);
+			String estat="tancat";
+			contenidor[posicio]=new Refrigerant(numSerie,estat,capacitat,mercaderia,temperatura);
+		}
+		else{
+			System.out.println("No i caben les mercaderias");
+		}
+	}
+
 }
